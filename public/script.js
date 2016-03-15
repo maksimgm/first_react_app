9b75d889350de437d280fd9d997ec4edb74deaae
maksimgm <maksimmamrikov@gmail.com>
2016-03-15 12:01:46 -0700

    add onclick function for editing and removing note

--- public/script.js
+++ public/script.js
@@ -1,10 +1,19 @@
 var Note = React.createClass({
+  edit: function(){
+    alert('editing note')
+  },
+  remove: function(){
+    alert('removing note')
+  },
   render: function(){
     return(
       <div className="note">
+        <p>{this.props.children}</p>
         <span>
-          <button className="btn btn-primary glyphicon glyphicon-pencil"/>
-          <button className="btn btn-danger glyphicon glyphicon-trash"/>
+          <button onClick={this.edit}
+            className="btn btn-primary glyphicon glyphicon-pencil"/>
+          <button onClick={this.remove}
+            className="btn btn-danger glyphicon glyphicon-trash"/>
         </span>
       </div>
     );
